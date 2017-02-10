0fd1f33021637624eedc9a211138d63215430903
Thecrazyskull <anaskarbila@gmail.com>
2017-02-10 09:38:50 -0500

    Launcher3: Double tap on home screen to turn off screen
    This squashes the following commits:
    Author: DennySPB <dennyspb@gmail.com>
    Date:   Fri Sep 27 15:21:57 2019 +0300
    
        Launcher3: Add toggle for DT2S
    
        commit 39fa8b063cf682771c1800a2391689f96808218f
        Author: Omkar Chandorkar <gotenksIN@aosip.dev>
        Date:   Tue Feb 1 19:06:26 2022 +0530
    
            fixup! Launcher3: Add toggle for DT2S
    
            Change-Id: I2e537fa8641503d4ddfb10cad79e7d5fd0db3f2b
    
        Signed-off-by: DennySPb <dennyspb@gmail.com>
        Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
        Change-Id: I22abb02de7394094653d571e20eee0e93fb1e9a5
    
    Change-Id: I554c005d5e523aca0842c78a353686e86af1a7f2
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -108,6 +108,9 @@ import java.util.List;
 import java.util.Locale;
 import java.util.function.Predicate;
 
+import android.content.SharedPreferences;
+import com.android.launcher3.LauncherPrefs;
+
 /**
  * Various utilities shared amongst the Launcher's classes.
  */
@@ -137,6 +140,8 @@ public final class Utilities {
      * Set on a motion event dispatched from the nav bar. See {@link MotionEvent#setEdgeFlags(int)}.
      */
     public static final int EDGE_NAV_BAR = 1 << 8;
+
+    public static final String KEY_DT_GESTURE = "pref_dt_gesture";
 
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
@@ -935,5 +940,10 @@ public final class Utilities {
     public static boolean isWorkspaceEditAllowed(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return !prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_LOCK, false);
+    }
+
+    public static boolean isDoubleTapGestureEnabled(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DT_GESTURE, true);
     }
 }
